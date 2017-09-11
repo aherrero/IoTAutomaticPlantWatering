9fe90b2b2ca67fe31785a878a630fc9d362caf94
aherrero <alexhc89@gmail.com>
2017-09-11 07:11:47 +0200

    Second prototype, measuring the soil moisture and activating pump motor if needed

---
+++ IoTAutomaticPlantWatering.ino
@@ -0,0 +1,97 @@
+#include <LowPower.h>       //Library to sleep the micro
+
+/*
+ * Input/Output
+ */
+
+int inMoistureSensor = 1;    //Analog: Read Sensor
+int pinOnMoistureSensor = 2; //Digital: on/off sensor
+int outControl = 3;          //PWM Digital: Motor output
+int pinInternalLed = 13;     //Digital: Led output (internal) used as indicator
+
+/*
+ * Var
+ */
+ 
+int motorSpeed = 255;
+
+/*
+ * Functions
+ */
+ 
+int getMoistureValue()
+{
+  // sensor temperature
+  int sensorMoisture = analogRead(inMoistureSensor);  
+  //Serial.print(sensorMoisture); Serial.println(" raw value.");
+  return sensorMoisture;
+}
+ 
+/*
+ * setup()
+ */
+void setup()
+{
+  Serial.begin(9600);                   //Start the serial connection with the computer
+  
+  pinMode(pinOnMoistureSensor, OUTPUT); //pin to power on the sensor
+  pinMode(outControl, OUTPUT);          //pin to control de pump motor
+  
+  pinMode(pinInternalLed, OUTPUT);      //led to show if the pump motor is working
+}
+
+
+/*
+ * loop()
+ */
+void loop()                     // run over and over again
+{  
+  //Show the micro wakes up
+  digitalWrite(pinInternalLed, HIGH);
+  
+  //Power on the moisture Sensor
+  digitalWrite(pinOnMoistureSensor, HIGH);
+  delay(100);
+    
+  //Read Sensor
+  int sensorMoisture = getMoistureValue();
+  
+  //Action
+  int firstAction = 0;
+  while(sensorMoisture < 800)   //emperic value to determinate if the plant needs water
+  {
+    //Notify the pump motor is running with blink
+    digitalWrite(pinInternalLed, LOW);
+    delay(100);
+    
+    //Run pump motor
+    if(firstAction == 0)
+    {
+      analogWrite(outControl, motorSpeed);
+      firstAction = 1;
+    }
+    
+    //Update sensor value
+    sensorMoisture = getMoistureValue();
+    
+    //Blink
+    digitalWrite(pinInternalLed, HIGH);
+    delay(100);
+  }
+  
+  //Power off pump motor
+  analogWrite(outControl, 0);
+  
+  //Show the micro is going to wait
+  digitalWrite(pinInternalLed, LOW);
+  
+  //Power off the moisture Sensor
+  digitalWrite(pinOnMoistureSensor, LOW);
+  delay(100);
+
+  //Main Sleep
+  delay(2000);
+
+  //Sleep for 8s
+  //LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # IoTAutomaticPlantWatering
+The goal of this project is to automate the plant watering, measuring the soil moisture and activating the pump motor if the plant needs water.
